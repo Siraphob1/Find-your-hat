11235fc8440ec3c9e70dba51b9252715a1b7d4f2
Siraphob1 <Sirapop_t@hotmail.com>
2023-08-28 10:30:38 +0700

    default code

---
+++ main.js
@@ -0,0 +1,22 @@
+// Please copy and paste your GitHub Repo on line 2 (optional)
+// <GitHub Repo>
+
+// JavaScript Assessment Rubric: https://generation.instructure.com/courses/2342/assignments/143783
+
+// Codecademy: https://www.codecademy.com/paths/front-end-engineer-career-path/tracks/fecp-javascript-syntax-part-iii/modules/fecp-challenge-project-find-your-hat/projects/find-your-hat
+
+// Please break down your thinking process step-by-step (mandatory)
+// step 1 :
+
+
+
+// JS Assessment: Find your hat //
+// const prompt = require('prompt-sync')({sigint: true});
+
+//Set symbol 
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+
