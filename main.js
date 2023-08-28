cfb58f3e68325e5743a40b77e4f234c0e4742c5d
Siraphob1 <Sirapop_t@hotmail.com>
2023-08-28 11:11:26 +0700

    step1-3  and step sum

--- main.js
+++ main.js
@@ -11,7 +11,8 @@
 
 
 // JS Assessment: Find your hat //
-// const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({sigint: true});
+const clear = require('clear-screen');
 
 //Set symbol 
 const hat = '^';
@@ -20,3 +21,43 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 
+//Step1 Create class Field
+class Field{
+    constructor(){
+        this.field_row = 3
+        this.field_column = 3
+        this.player ={
+            position_x: 0,
+            position_y: 0
+        }
+        this.is_playing = false
+    }
+
+    //step 2 Start game
+    StartGame(){
+        clear()
+        console.log('\nWelcome to Find-Your-Hat Game 🧢');
+        let input_player = prompt('Press p for play game: ');
+        if(input_player === 'p' || input_player === 'P') this.is_playing = true;       
+    }
+
+    //step 3 player define row & column
+    DefineFieldSize(){
+        let input_row = prompt('please specifie count of row: ');
+        let input_column = prompt('please specifie count of column: ');
+        this.field_row = input_row;
+        this.field_column = input_column;
+    }
+
+    //step sum  total method
+    PlayGame(){
+        this.StartGame()
+        this.DefineFieldSize()
+    }
+
+
+}
+
+let object1 = new Field()
+object1.PlayGame()
+console.log(object1)
